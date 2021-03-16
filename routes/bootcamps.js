45650e032615a0db18472d672dd21eca08d873a2
Robert Woodruff <rwoodruff@Roberts-Air.fios-router.home>
2021-03-15 21:03:40 -0400

    added comments to routes/bootcamps.js and server.js

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,7 @@
+// require express app to be used
 const express = require('express');
+
+// importing multiple express routes to update the page
 const {
   getBootcamps,
   getBootcamp,
@@ -9,18 +12,24 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+// importing express routes
 const Bootcamp = require('../models/Bootcamp');
 
+// requires files from routes foldder
 const courseRouter = require('./courses');
 const reviewRouter = require('./reviews');
 
+// used to handled object requests
 const router = express.Router();
 
+// importing express routes
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
+// adding routes to express app
 router.use('/:bootcampId/courses', courseRouter);
 router.use('/:bootcampId/reviews', reviewRouter);
+
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
--- server.js
+++ server.js
@@ -14,46 +14,65 @@ const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
+// points to the path of the enviroment variable file which stores secret/sensitve information rather than in your code
+// configuring eviroment variables
 dotenv.config({ path: './config/config.env' });
 
+// connects to database
 connectDB();
 
+//importing express routes
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
 const reviews = require('./routes/reviews');
 
+// instanitating the express app
 const app = express();
 
+// tells express to interpret incoming request as JSON
+// this is built in middelware
 app.use(express.json());
 
+// places all the cookies  in the req into req.cookies
 app.use(cookieParser());
 
+// configure development enviroment (not production)
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// required to allow client to upload files 
 app.use(fileupload());
 
+// removes specific operator keys to enable pass through the mongo db and prevent user interference
+// protects against sql injections
 app.use(mongoSanitize());
 
+// helps to secure express apps by sending various http headers for virus protection
 app.use(helmet());
 
+// a module used to filter input from users to prevent cross site scripting (xss) and command injection attacks
 app.use(xss());
 
+// sets the parameters for the use of the limiter
+// limits the number of requests made per millisec
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 100
 });
 app.use(limiter);
 
+// prevent http perimeter polution 
 app.use(hpp());
 
+// enabling cross origin resource sharing
 app.use(cors());
 
+ // serves static files in public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// adding routes to express app
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
